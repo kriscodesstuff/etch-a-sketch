1aae7db9120668c443b33083ff37bbf4cc7b3a5d
Kristin <89033364+kriscodesstuff@users.noreply.github.com>
2023-07-16 17:33:40 -0500

    Finish createGrid function

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log('hello');
+const container = document.querySelector('#container');
+
+function createGrid(num){
+    const numOfSquares = num * (num + 1) * (2 * num + 1) / 6;
+
+    for(let i = 0; i <= numOfSquares; i++){
+
+        container.append(document.createElement('div'));
+    }
+    
+}
+
+createGrid(16);
