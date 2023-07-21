e5a8db7cfd881e05f8b1acf8af0181f36ee704df
Kristin <89033364+kriscodesstuff@users.noreply.github.com>
2023-07-21 13:55:22 -0500

    Add hover effect

--- script.js
+++ script.js
@@ -1,15 +1,27 @@
-const container = document.querySelector('#container');
+const gridContainer = document.querySelector('#grid-container');
+const button = document.querySelector('button');
 
 function createGrid(num){
     const numOfSquares = num * num;
     for(let i = 0; i <= numOfSquares; i++){
         const gridItem = document.createElement('div');
         gridItem.classList.add('grid-item')
-        container.append(gridItem);
-        container.style.gridTemplateRows = `repeat(${num}, 1fr)`;
-        container.style.gridTemplateColumns = `repeat(${num}, 1fr)`;
+        gridContainer.append(gridItem);
+        gridContainer.style.gridTemplateRows = `repeat(${num}, 1fr)`;
+        gridContainer.style.gridTemplateColumns = `repeat(${num}, 1fr)`;
     }
     
 }
 
 createGrid(16);
+
+function addHoverClass(e){
+    e.target.classList.add('hovered');
+}
+
+const gridItem = document.querySelectorAll('.grid-item');
+
+gridItem.forEach((item) => {
+    item.addEventListener('mouseover', addHoverClass);
+})
+
