8311ef671bd122fcb67b0fba18ee9c29cadd71cd
Kristin <89033364+kriscodesstuff@users.noreply.github.com>
2023-07-23 20:17:07 -0500

    Add user input

--- script.js
+++ script.js
@@ -1,27 +1,46 @@
 const gridContainer = document.querySelector('#grid-container');
 const button = document.querySelector('button');
 
+function addHoverClass(e){
+    e.target.classList.add('hovered');
+}
+
 function createGrid(num){
     const numOfSquares = num * num;
-    for(let i = 0; i <= numOfSquares; i++){
+    for(let i = 0; i < numOfSquares; i++){
         const gridItem = document.createElement('div');
         gridItem.classList.add('grid-item')
         gridContainer.append(gridItem);
         gridContainer.style.gridTemplateRows = `repeat(${num}, 1fr)`;
         gridContainer.style.gridTemplateColumns = `repeat(${num}, 1fr)`;
     }
+
+    const gridItem = document.querySelectorAll('.grid-item');
+
+    gridItem.forEach((item) => {
+    item.addEventListener('mouseover', addHoverClass);
+})
     
 }
 
 createGrid(16);
 
-function addHoverClass(e){
-    e.target.classList.add('hovered');
+function resetGrid(){
+    while(gridContainer.firstChild) {
+        gridContainer.removeChild(gridContainer.firstChild);
+    }
 }
 
-const gridItem = document.querySelectorAll('.grid-item');
 
-gridItem.forEach((item) => {
-    item.addEventListener('mouseover', addHoverClass);
-})
+function getGridNum(){
+    resetGrid();
+    const num = prompt('Choose your grid number');
+    if(num > 1 && num <= 100){
+        createGrid(num);
+    }else{
+        alert('Invalid Answer');
+        createGrid(16);
+    }
+}
 
+button.addEventListener('click', getGridNum);
